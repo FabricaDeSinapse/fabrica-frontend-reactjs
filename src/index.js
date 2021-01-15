24e4a8f09fbdc339cd25688bb98119900e3d102b
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-15 19:29:45 -0300

    Arquivos da pasta src removidos e estrutura básica com estilização adicionada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+      <div className="board-row">
+        <button className="square"></button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+      <div className="board-row">
+        <button className="square"></button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+    </div>
+  )
+}
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board/>
+      </div>
+
+      <div className="game-info">
+        Game Info
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
